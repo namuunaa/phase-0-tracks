0256498731388508ee486968219f659ca1344f9e
Namuun Bayaraa <namuka@gmail.com>
2017-01-25 19:10:31 -0800

    Interview vampire: Collect data

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year_born = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?(Y/N)"
+insurance = gets.chomp
+
