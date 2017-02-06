086a1d792bb891499197b1aea0fc26b2bc6d92a4
Namuun Bayaraa <namuka@gmail.com>
2017-02-05 16:58:27 -0800

    Pseudocode bubble sorting method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -28,3 +28,41 @@ end
 
 p fib(6)
 p fib (100)
+
+=begin
+Pseudocode a bubble sorting method. 
+-Define a method that takes an array as a variable
+-Save the length of the array in a new variable
+-Create a loop that iterates through the array 1 times less than
+the array length
+-Give a conditional statement swapped to be false at the beginning
+of the loop
+-Compare an array item with the next item in the array,
+and if the array item is greater than the next item, swap their position
+-Break the loop when swapped = false, and the array is finished sorting
+
+=end
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |int|
+      if array[int] > array[int+1]
+        array[int], array[int+1] = array[int+1], array[int]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+numbers = [13, 2, 45, 23, 1, 4]
+p bubble_sort(numbers)
+
+
+
