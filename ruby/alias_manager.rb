cf40b663d100fe5c35eebc19bf61dc079b09ae84
Namuun Bayaraa <namuka@gmail.com>
2017-02-05 21:25:16 -0800

    Add method to swap letters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,51 +1,59 @@
 =begin
  	 
-Prompt the user to input their full name
-Create a method that swaps their first and last name
-Create a method that changes all the vowels to the next vowel in 'aeiou'
-Create a method that changes all the rest of consonants to the next
-
+-Create a method that swaps their first and last name
+-Create a method that changes all the vowels to the next vowel in 'aeiou'
+	-Create a loop that goes through all the letters of the name
+	-When the letter matches a letter in vowels list, 
+	change it to the next vowel in the vowels list
+	-Until all the letters are swapped, do the loop
+	-Save the newly swapped name in a variable
+-Create a method that changes all the rest of consonants to the next
+	-Iterate through all the letters and change the letter to the next one
+	in the consonants list.
+-Get user input for name and give the fake name as output until they say quit.
+-Print all the data user entered
 =end
 
-real_name = 'Felicia Torres'
 
-swap_name = real_name.split(' ').reverse.join(' ')
+real_name = "Felicia Torres"
+swap_name = real_name.split(" ").reverse.join(" ")
 
-def next_vowel(swap_name)
-	vowels = "aeiou"
+#Change vowels in name to next vowels
+def next_vowel(name)
+	vowels = ["a", "e", "i", "o", "u"]
 	index = 0
-	while index < swap_name.length 
-		if swap_name.[index] == "u"
-			swap_name[index] == "a"
-		else 
-			swap_name[index] = vowels[index].next!
+	name = name.split('') # string to array
+	name_change1 = name.map do |letter| 
+		if vowels.include? letter 
+			vowels.rotate(1)[vowels.index(letter)]
+		else
+			letter
 		end
-	index+=1
 	end
-	swap_name
+	name_change1.join  # new array back to string
+end
+
+#p next_vowel("Felicia")
+
+#Change the consonants to next consonant
+def next_consonant(name)
+	consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", 
+		"m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+	index = 0
+	name = name.split('') # string to array
+	name_change2 = name.map do |letter| 
+		if consonants.include? letter 
+			consonants.rotate(1)[consonants.index(letter)]
+		else
+			letter
+		end
+	end
+	name_change2.join
 end
 
 
-=begin
-def next_consonant(swap_name)
-	consonants = "bcdfghjklmnpqrstvwxyz"
-	index = 0
-	while index < swap_name.length 
-		until swap_name[index] == consonants[n]
-			n+=1
-		end
-		if swap_name[index] == "z"
-			swap_name[index] == "b"
-		else 
-		swap_name[index] = consonants[index].next!
-		end
-	index+=1
-	end
-	puts swap_name
-end
-
-
-=end
+p next_vowel("Felicia")
+p next_consonant("Felicia")
 
 
 
