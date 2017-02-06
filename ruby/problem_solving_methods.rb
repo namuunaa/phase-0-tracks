d20153ef699acd3d0896cbfe0b6f9669eddd6761
Namuun Bayaraa <namuka@gmail.com>
2017-02-05 16:27:27 -0800

    Add method that generates array of fibonacci numbers with given number

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -17,3 +17,14 @@ p search_array(arr, 89)
 p search_array(arr, 24)
 
 #calculate fibonacci number
+
+def fib(num)
+	fib_num = [0, 1]
+	until fib_num.length == num
+		fib_num << (fib_num[-1] + fib_num[-2])
+	end
+	return fib_num
+end
+
+p fib(6)
+p fib (100)
