c1143f3b2c6dfc3a60dba95b79720726a1a44b41
Namuun Bayaraa <namuka@gmail.com>
2017-02-07 13:09:14 -0800

    Design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+CLASS ------
+Puppy
+
+CHARACTERISTICS ------
+Color: varies
+Eyes: watery
+Size: varies
+Friendly: yes
+Intelligence: varies
+Shedding: varies
+
+BEHAVIOR ------
+Jump
+Fetch
+Play
+Roll over
+=end
