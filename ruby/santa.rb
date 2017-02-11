f164813918dc17583217ae0816d25a42afabd376
Namuun Bayaraa <namuka@gmail.com>
2017-02-11 15:44:11 -0800

    Process santa simulator with user input

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,11 @@
+#SANTACON SIMULATOR
 class Santa
-  attr_reader :age, :ethnicity
-  attr_accessor :gender, :reindeer_ranking
-  
-  def initialize(gender, ethnicity)
-    puts "Initializing Santa instance ..."
-    @gender = gender
-    @ethnicity = ethnicity
+  attr_accessor :gender, :reindeer_ranking, :age, :ethnicity
+
+  def initialize
+    #puts "Initializing Santa instance ..."
+    @gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+    @ethnicity = ["black", "Latino", "white", "Japanese-African", "Native-American", "Aboriginal", "Asian", "Mystical Creature (unicorn)", "N/A"]
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
  
@@ -23,7 +23,7 @@ class Santa
     @age = 1
   end
   
-  def get_mad_at(reindeer_name)
+  def get_mad_at(reindeer_name) 
     @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name)))
   end
   #   #setter method
@@ -41,21 +41,36 @@ class Santa
 
 end
 
-#DRIVER CODE
-santa = Santa.new("female", "Mongolian")
-santa.speak
-santa.eat_milk_and_cookies("oreo")
-santa.celebrate_birthday
-puts "Santa turned #{santa.celebrate_birthday}"
-santa.get_mad_at("Dancer")
-puts santa.gender = "gender-fluid"
-#add diverse initializations
-santas = []
-genders = ["female", "male", "gender-fluid", "n/a"]
-ethnicities = ["white", "black", "multiracial", "hispanic"]
-genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
+# #DRIVER CODE
+# santa = Santa.new("female", "Mongolian")
+# santa.speak
+# santa.eat_milk_and_cookies("oreo")
+# santa.celebrate_birthday
+# puts "Santa turned #{santa.celebrate_birthday}"
+# santa.get_mad_at("Dancer")
+# puts santa.gender = "gender-fluid"
+# #add diverse initializations
+# santas = []
+# genders = ["female", "male", "gender-fluid", "n/a"]
+# ethnicities = ["white", "black", "multiracial", "hispanic"]
+# genders.length.times do |i|
+#   santas << Santa.new(genders[i], ethnicities[i])
+# end
+
+puts "How many Santas would you like to generate?"
+santa_count = gets.chomp.to_i
+
+santa_count.times do 
+  santa = Santa.new
+  santa.gender = santa.gender.sample
+  santa.ethnicity = santa.ethnicity.sample
+  santa.age = rand(140)
+  puts "Santa created is #{santa.age} years old, #{santa.gender} and of #{santa.ethnicity} decent."
+  puts santa.speak
 end
+
+
+
 
 
 
