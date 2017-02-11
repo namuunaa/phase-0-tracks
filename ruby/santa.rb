b5abde14367b81755c65181a781dcdc82f44962f
Namuun Bayaraa <namuka@gmail.com>
2017-02-11 13:27:15 -0800

    Add driver code to test class method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,17 +1,30 @@
 class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+ 
+  end
+
   def speak
-    print "Ho, ho, ho! Haaaappy holidays!"
+    puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie_type)
-    print "That was a good #{cookie_type}!" 
-  end
-
-  def initialize
-    "Initializing Santa instance ..."
+    puts "That was a good #{cookie_type}!" 
   end
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("oreo")
+#Driver Code
+#santa = Santa.new("female", "Mongolian")
+#santa.speak
+#santa.eat_milk_and_cookies("oreo")
+santas = []
+genders = ["female", "male", "gender-fluid", "n/a"]
+ethnicities = ["white", "black", "multiracial", "hispanic"]
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+end
+
