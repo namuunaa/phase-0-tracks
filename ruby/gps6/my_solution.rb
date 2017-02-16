031deca470099e06045ec1afc7c9224514dc0df3
Namuun Bayaraa <namuka@gmail.com>
2017-02-15 20:45:12 -0800

    Rfector methods in more DRY way, and add comments of reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,21 +1,25 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [with: Parth Patel].
+# We spent 1.5 hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Require relative allows you to load another file that is relative 
+# to the file containing the require_relative
+# Require is for built in methods, will search your ruby gems for that file
+
 require_relative 'state_data'
 
 class VirusPredictor
 
+  # Initializes instances of the class with given parameters
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # Combines the results of predicted_deaths and speed_of_spread
   def virus_effects
     predicted_deaths
     speed_of_spread 
@@ -23,24 +27,44 @@ class VirusPredictor
 
   private
 
-  def predicted_deaths
+  # Takes the population density data, and if the condition is met 
+  # sets the number of deaths to a certain number
+  #def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
+  #   if @population_density >= 200
+  #     number_of_deaths = (@population * 0.4).floor
+  #   elsif @population_density >= 150
+  #     number_of_deaths = (@population * 0.3).floor
+  #   elsif @population_density >= 100
+  #     number_of_deaths = (@population * 0.2).floor
+  #   elsif @population_density >= 50
+  #     number_of_deaths = (@population * 0.1).floor
+  #   else
+  #     number_of_deaths = (@population * 0.05).floor
+  #   end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+  #   print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
+  # end
+
+  def predicted_deaths#(population_density, population, state)
+  pct = case
+  when @population_density >= 200 
+  0.4
+  when @population_density >= 150 
+  0.3
+  when @population_density >= 100 
+  0.2
+  when @population_density >= 50 
+  0.1
+  else
+  0.05
+  end
+  print "#{@state} will lose #{ (@population * pct).floor } people in this outbreak"
   end
 
+=begin
+  # Increment the speed of spread based on population density condition
   def speed_of_spread 
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -57,12 +81,28 @@ class VirusPredictor
     else
       speed += 2.5
     end
-
     puts " and will spread across the state in #{speed} months.\n\n"
 
   end
-
+=end
+def speed_of_spread
+  speed = 0.0
+speed_hi = case
+  when @population_density >= 200 
+  0.5
+  when @population_density >= 150 
+  1
+  when @population_density >= 100 
+  1.5
+  when @population_density >= 50 
+  2
+  else
+  2.5
+  end
+  puts " and will spread across the state in #{speed += speed_hi} months.\n\n"
+  end
 end
+
 
 #=======================================================================
 
@@ -89,3 +129,22 @@ end
 
 #=======================================================================
 # Reflection Section
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# - First hash is given in a string, second hash is given in symbols
+# What does require_relative do? How is it different from require?
+# - Require relative allows you to load another file that is relative 
+#   to the file containing the require_relative
+#   Require is for built in methods, will search your ruby gems for that file
+# What are some ways to iterate through a hash?
+# - Use a loop do with a block 
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# Everytime we create a method, we don't have to set the variables as long as we defined it in the initialize method
+# What concept did you most solidify in this challenge?
+# - Reading a code and refactoring using other built in methods
+# - Accessing a hashy hash
+
+
+
+
+
+
