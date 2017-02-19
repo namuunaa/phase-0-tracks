0710bc8a6a016bcefcb9a8e0131347bf514f2222
Namuun Bayaraa <namuka@gmail.com>
2017-02-18 23:29:18 -0800

    Define a function that checks matching key-value pair in 2 objects

--- js/algos.js
+++ js/algos.js
@@ -9,7 +9,7 @@
 //    -If not do nothing, keep the longest word as the default
 // -Print the longest word/phrase
 
-function findLongestWord(array) {
+var findLongestWord = function (array) {
 
   var longestWord = array[0];
   for (var i = 0; i < array.length; i++) {
@@ -30,14 +30,24 @@ console.log(findLongestWord(["hello my darling", "hello", "hi, dear", "hi"]));
 // find a key-value match in 2 objects
 // PSEUDOCODE:
 // Create a function that takes 2 objects
-//  - Declare 2 empty objects in new variable s
+//  - Use for loop to go over all the keys in the first object
 //  - Add a conditional statement that checks whether 2 objects share a key - value pair:
-//     - Use bracket notation to access a key's value.
-//     - Use "==" to compare
+//     - Use bracket notation to access the properties's value.
+//     - Use "==" to compare object 1 property value with object 2's value
 //     - Return true, if there's a match
-//     - Else, if there is no match, return false
+//     - If there is no match, return false
 
+var shareKeyValue = function (object1, object2) {
+  for (var key in object1) {
+    if (object1[key] == object2[key]) {
+     return true;
+    } 
+  }
+  return false;
+}
 
+console.log(shareKeyValue({name: "Steven", age: 54},{name: "Tamir", age: 54}));
+console.log(shareKeyValue({name: "Lola", gender: "female"},{name: "Mark", gender: "male"}))
 
 
 
