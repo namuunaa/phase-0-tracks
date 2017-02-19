1dea279b615cd92e2801abf22e755a991291ce15
Namuun Bayaraa <namuka@gmail.com>
2017-02-18 19:49:20 -0800

    Create a function to find the longest phrase in an array

--- js/algos.js
+++ js/algos.js
@@ -2,26 +2,26 @@
 // PSEUDOCODE
 // -Create an empty array to start
 // -Declare a function that takes the array:
-//    -Set an empty variable for the longest word/phrase 
+//    -Set the first item in the array as the longest word/phrase variable
 //    -Do a loop that goes through each word/phrase length in the array
 //    -Compare each length with the longest word/phrase variable
 //    -If the length is longer, then change the value of the longest word/phrase to new value
 //    -If not do nothing, keep the longest word as the default
 // -Print the longest word/phrase
 
-function Word(array) {
+function findLongestWord(array) {
 
-  this.array = array;
-  this.findLongestWord = function() {  
-  var longest_word = "";
+  var longestWord = array[0];
   for (var i = 0; i < array.length; i++) {
-    if (array[i].length > longest_word.length)
-      longest_word = array[i];
-    else
-      longest_word; 
+    if (array[i].length > longestWord.length) {
+      longestWord = array[i];
+    }
+    else {
+      longestWord; 
+    }
   }
-  };
+  return longestWord;
 }
-var newArray = new Word("long phrase","longest phrase","longer phrase");
-console.log(newArray.findLongestWord())
-// not working ,come back to it
+
+console.log(findLongestWord(["long phrase","longest phrase","longer phrase"]));
+console.log(findLongestWord(["hello my darling", "hello", "hi, dear", "hi"]));
