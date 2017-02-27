00f3a258842a837cf204622fdb97c6e2631a8598
Namuun Bayaraa <namuka@gmail.com>
2017-02-27 00:11:40 -0800

    Create a table

--- databases/grocery_list.rb/grocery_list.rb
+++ databases/grocery_list.rb/grocery_list.rb
@@ -1,5 +1,22 @@
 # Grocery shopping list
-
+# Create a dataase of shopping list items.
 require 'sqlite3'
 
-db = SQLITE3::Database.new("grocery.db")
+db = SQLite3::Database.new("grocery.db")
+
+create_table_grocery = <<-SQL
+  CREATE TABLE IF NOT EXISTS grocery(
+    id INTEGER PRIMARY KEY,
+    item VARCHAR(255),
+    quantity INT,
+    price INT
+  )
+SQL
+
+=begin
+db.execute(create_table_grocery)
+
+def add_items(item, quantity, price)
+  db.execute("INSERT INTO grocery (item, quantity, price) VALUE (?, ?, ?)", [item, quantity, price])
+end
+=end
