8f006e4a297c0f9036cab5ca636137f99fb853a5
Namuun Bayaraa <namuka@gmail.com>
2017-02-27 01:00:44 -0800

    Add a method to add items into grocery list

--- databases/grocery_list.rb/grocery_list.rb
+++ databases/grocery_list.rb/grocery_list.rb
@@ -1,5 +1,6 @@
 # Grocery shopping list
-# Create a dataase of shopping list items.
+# Create a database of shopping list items.
+
 require 'sqlite3'
 
 db = SQLite3::Database.new("grocery.db")
@@ -8,15 +9,29 @@ create_table_grocery = <<-SQL
   CREATE TABLE IF NOT EXISTS grocery(
     id INTEGER PRIMARY KEY,
     item VARCHAR(255),
-    quantity INT,
-    price INT
+    quantity INT
   )
 SQL
 
-=begin
 db.execute(create_table_grocery)
 
-def add_items(item, quantity, price)
-  db.execute("INSERT INTO grocery (item, quantity, price) VALUE (?, ?, ?)", [item, quantity, price])
+def add_items(db, item, quantity)
+  db.execute("INSERT INTO grocery (item, quantity) VALUES (?, ?)", [item, quantity])
 end
-=end
+
+# User Interface
+
+loop do 
+  puts "what item would you like to add to your grocery cart? Type done when finished"
+  grocery_item = gets.chomp
+
+  break if grocery_item == "done"
+
+  puts "How many do you need?"
+  grocery_count = gets.chomp.to_i
+
+  add_items(db, grocery_item, grocery_count)
+
+end
+
+
